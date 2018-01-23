4d06de5924c627a4e05c11277e918548a2754876
Jbelles <jgbelles@gmail.com>
2018-01-23 10:23:46 -0800

    Update UnitTest1.cs

--- Tweetinvi.Tests/UnitTest1.cs
+++ Tweetinvi.Tests/UnitTest1.cs
@@ -15,12 +15,12 @@ namespace TwitterDemo.Tests
                 //Account is @JarretKoder (jarret@koder.ai)
                 //PW is KoderDevAccount
                 //These 2 keys are specific to Jarret's Developer App
-                "XYM4ekZ26J4WLLyEhJ5YXzgB5",
-                "GuLBBQCnknzltZZImHbC15mzJZx6gfneeiagrb3Nrf33Xb4zoN",
+                "<Key1>",
+                "<Key2>",
 
-                //these 2 keys are specific to Jarret's twitter account
-                "798968006663507969-TadVbbNzNCGa2P9E8iFC8OTwT3go8qa",
-                "90qWXFITcarDrOe3gkvjEveLODnbiZlMeQIhuBJUb2vl1"
+                //these 2 keys are specific to your twitter account
+                "<public>",
+                "<private>"
     );
         }
 
